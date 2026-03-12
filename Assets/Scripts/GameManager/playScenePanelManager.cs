e610a7768fe01130b3d817eabf4ab8259fa4254e
doom99987 <126832091+doom99987@users.noreply.github.com>
2026-03-12 14:10:57 -0700

    Play Scene panels
    win panel
    death panel
    shop panel

---
+++ Assets/Scripts/GameManager/playScenePanelManager.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class playScenePanelManager : MonoBehaviour
+{
+    [SerializeField] GameObject shopPanel;
+    [SerializeField] GameObject deathPanel;
+    [SerializeField] GameObject victoryPanel;
+    public void openShopPanel()
+    {
+        shopPanel.SetActive(true);
+    }
+    public void openDeathPanel()
+    {
+        deathPanel.SetActive(true);
+    }
+    public void openVictoryPanel()
+    {
+        deathPanel.SetActive(true);
+    }
+    public void closeAllPanel()
+    {
+        shopPanel.SetActive(false);
+        deathPanel.SetActive(false);
+        victoryPanel.SetActive(false);
+    }
+    public void closeShopPanel()
+    {
+        shopPanel.SetActive(false);
+    }
+    public void closeDeathPanel()
+    {
+        deathPanel.SetActive(false);
+    }
+    public void closeVictoryPanel()
+    {
+        victoryPanel.SetActive(false);
+    }
+}
