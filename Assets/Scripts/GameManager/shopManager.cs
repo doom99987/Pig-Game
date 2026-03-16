b14e1283344e2b395edd11060c0bd95bf771dd61
doom99987 <126832091+doom99987@users.noreply.github.com>
2026-03-16 13:39:09 -0700

    upgrades

--- Assets/Scripts/GameManager/shopManager.cs
+++ Assets/Scripts/GameManager/shopManager.cs
@@ -4,45 +4,57 @@ using UnityEngine;
 
 public class shopManager : MonoBehaviour
 {
-    int count = 0;
+    protected int sCount = 0;
+    protected int hCount = 0;
+    protected int healingCount = 0;
+    protected int pierceCount = 0;
     [Header("References")]
     [SerializeField] GameObject gameManager;
     [SerializeField] GameObject player;
     [SerializeField] TextMeshProUGUI buySpeedText;
-   public enum UpgradeType
-    {
-        Speed,
-        Hp,
-        healing,
-        Pierce
-    }
     [SerializeField] float[] speedCost = { 5, 50, 500, 5000 };
-    [SerializeField]
-    //public void buyUpgrade(UpgradeType type)
-    //{
-    //    switch (type)
-    //    {
-    //        case UpgradeType.Speed:
-    //            buySpeed();
-    //            break;
-    //        case UpgradeType.Hp:
-    //            break;
-    //        case UpgradeType.healing:
-    //            break;
-    //        case UpgradeType.Pierce:
-    //            break;
-    //        default:
-    //            throw new ArgumentOutOfRangeException(nameof(type), type, null);
-    //    }
-    //}
+    [SerializeField] float[] hpCost = { 5, 50, 500, 5000 };
+    [SerializeField] float[] healingCost = { 5, 50, 500, 5000 };
+    [SerializeField] float[] pierceCost = { 5, 50, 500, 5000 };
     public void buySpeed()
     {
-                if (gameManager.GetComponent<moneyManager>().getMoney() >= speedCost[count])
+                if (gameManager.GetComponent<moneyManager>().getMoney() >= speedCost[sCount])
                 {
                     player.GetComponent<PlayerMovement>().setCurSpeed(player.GetComponent<PlayerMovement>().getCurrentSpeed() + 1);
-                    gameManager.GetComponent<moneyManager>().removeMoney(speedCost[count]);
-                    buySpeedText.text = $"Buy ${speedCost[count + 1] / 100f}";
-                    count++;
+                    gameManager.GetComponent<moneyManager>().removeMoney(speedCost[sCount]);
+                    buySpeedText.text = $"Buy ${speedCost[sCount + 1] / 100f}";
+                    sCount++;
                 }
+    }
+
+    public void buyHp()
+    {
+        if (gameManager.GetComponent<moneyManager>().getMoney() >= hpCost[hCount])
+        {
+            gameManager.GetComponent<HpManager>().upgradeMaxHp();
+            gameManager.GetComponent<moneyManager>().removeMoney(hpCost[hCount]);
+            buySpeedText.text = $"Buy ${hpCost[hCount + 1] / 100f}";
+            hCount++;
+        }
+    }
+    public void buyHealing()
+    {
+        if (gameManager.GetComponent<moneyManager>().getMoney() >= healingCost[healingCount])
+        {
+            player.GetComponent<PlayerMovement>().setCurSpeed(player.GetComponent<PlayerMovement>().getCurrentSpeed() + 1);
+            gameManager.GetComponent<moneyManager>().removeMoney(healingCost[healingCount]);
+            buySpeedText.text = $"Buy ${healingCost[healingCount + 1] / 100f}";
+            healingCount++;
+        }
+    }
+    public void buyPierce()
+    {
+        if (gameManager.GetComponent<moneyManager>().getMoney() >= pierceCost[pierceCount])
+        {
+            player.GetComponent<PlayerMovement>().setCurSpeed(player.GetComponent<PlayerMovement>().getCurrentSpeed() + 1);
+            gameManager.GetComponent<moneyManager>().removeMoney(pierceCost[pierceCount]);
+            buySpeedText.text = $"Buy ${pierceCost[pierceCount + 1] / 100f}";
+            pierceCount++;
+        }
     }
 }
