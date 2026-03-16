b103d011222302d37d719c575c61020e101560c4
doom99987 <126832091+doom99987@users.noreply.github.com>
2026-03-16 13:04:07 -0700

    shop more optimal

--- Assets/Scripts/GameManager/shopManager.cs
+++ Assets/Scripts/GameManager/shopManager.cs
@@ -1,3 +1,4 @@
+using System;
 using TMPro;
 using UnityEngine;
 
@@ -8,43 +9,15 @@ public class shopManager : MonoBehaviour
     [SerializeField] GameObject gameManager;
     [SerializeField] GameObject player;
     [SerializeField] TextMeshProUGUI buySpeedText;
+    [SerializeField] float[] speedCost = { 5, 50, 500, 5000 };
     public void buySpeed()
     {
-        switch (count)
-        {
-            case 0:
-                if (gameManager.GetComponent<moneyManager>().getMoney() >= 5)
+                if (gameManager.GetComponent<moneyManager>().getMoney() >= speedCost[count])
                 {
                     player.GetComponent<PlayerMovement>().setCurSpeed(player.GetComponent<PlayerMovement>().getCurrentSpeed() + 1);
-                    gameManager.GetComponent<moneyManager>().removeMoney(5);
-                    buySpeedText.text = "Buy 50";
+                    gameManager.GetComponent<moneyManager>().removeMoney(speedCost[count]);
+                    buySpeedText.text = $"Buy ${speedCost[count + 1] / 100f}";
                     count++;
                 }
-                break;
-            case 1:
-                 if (gameManager.GetComponent<moneyManager>().getMoney() >= 50)
-                {
-                    player.GetComponent<PlayerMovement>().setCurSpeed(player.GetComponent<PlayerMovement>().getCurrentSpeed() + 1);
-                    gameManager.GetComponent<moneyManager>().removeMoney(10);
-                    count++;
-                }
-                break;
-            case 2:
-                 if (gameManager.GetComponent<moneyManager>().getMoney() >= 500)
-                {
-                    player.GetComponent<PlayerMovement>().setCurSpeed(player.GetComponent<PlayerMovement>().getCurrentSpeed() + 1);
-                    gameManager.GetComponent<moneyManager>().removeMoney(15);
-                    count++;
-                }
-                break;
-            case 3:
-                if (gameManager.GetComponent<moneyManager>().getMoney() >= 5000)
-                {
-                    player.GetComponent<PlayerMovement>().setCurSpeed(player.GetComponent<PlayerMovement>().getCurrentSpeed() + 1);
-                    gameManager.GetComponent<moneyManager>().removeMoney(20);
-                    count++;
-                }
-                break;
-        }
     }
 }
