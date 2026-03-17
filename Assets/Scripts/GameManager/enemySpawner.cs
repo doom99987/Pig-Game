04e401b12d184d365df6eeaf40c2c92fc89c30c1
doom99987 <126832091+doom99987@users.noreply.github.com>
2026-03-17 13:48:41 -0700

    enemy spawner fixed

--- Assets/Scripts/GameManager/enemySpawner.cs
+++ Assets/Scripts/GameManager/enemySpawner.cs
@@ -3,27 +3,28 @@ using UnityEngine.Rendering;
 
 public class enemySpawner : MonoBehaviour
 {
+    [Header("Enemy Spawning Settings")]
     [SerializeField] protected GameObject enemyPrefab;
     [SerializeField] Transform[] spawnPoints;
-    protected float spawnDelay = 5f;
+    [SerializeField] protected float spawnDelay = 5f;
+    protected float nextSpawnTime = 5;
 
     private void Update()
     {
         if (!gameObject.GetComponent<gameManager>().getGameState())
         {
-            spawnDelay -= Time.deltaTime;
-            if (spawnDelay < 0)
+            if (Time.time > nextSpawnTime)
             {
                 spawnEnemy();
-                spawnDelay = 5f;
+                nextSpawnTime = Time.time + spawnDelay;
             }
         }
     }
 
     public void spawnEnemy()
     {
-        int random = Random.Range(1, spawnPoints.Length);
-        Instantiate(enemyPrefab, spawnPoints[random].position, Quaternion.identity);
+        int random = Random.Range(0, spawnPoints.Length);
+        Instantiate(enemyPrefab, spawnPoints[random].position, spawnPoints[random].rotation);
     }
 }
 
