7f071a6da09ebd146a3b1fd4a9d262fe0280b931
doom99987 <126832091+doom99987@users.noreply.github.com>
2026-03-18 12:49:00 -0700

    bullets

--- Assets/Scripts/GameManager/bullets.cs
+++ Assets/Scripts/GameManager/bullets.cs
@@ -3,5 +3,8 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "bullets", menuName = "Scriptable Objects/bullets")]
 public class bullets : ScriptableObject
 {
-    
+    [SerializeField] SpriteRenderer[] bulletSprites;
+    [SerializeField] protected int bulletDmg = 1;
+    [SerializeField] protected float bulletSpeed = 5f;
+    [SerializeField] protected int bulletPierce = 0;
 }
