8e456d7ecf7be6d60b618f5f1097563f597c4244
doom99987 <126832091+doom99987@users.noreply.github.com>
2026-03-20 13:13:06 -0700

    money scales per round on enemy death

--- Assets/Scripts/Enemy/enemyHp.cs
+++ Assets/Scripts/Enemy/enemyHp.cs
@@ -10,11 +10,12 @@ public class enemyHp : MonoBehaviour
     [Tooltip("Maximum HP of the enemy. (Not Used)")]
     [SerializeField] protected int maxHp = 3;
     [Tooltip("Money give to player on enemy kill (5 = $0.05)")]
-    [SerializeField] protected int moneyOnDeath = 5;
-
+    [SerializeField] protected int moneyOnDeath = 0;
+    [SerializeField] protected int mulitplier = 5;
     public void Start()
     {
         gameManager = GameObject.Find("gameManager");
+        moneyOnDeath = (gameManager.GetComponent<roundManager>().getRound() + 1) * mulitplier;
     }
     /// <summary>
     /// take 1 dmg
@@ -62,6 +63,7 @@ public class enemyHp : MonoBehaviour
     {
         hp = maxHp;
     }
+
     /// <summary>
     /// gives money to the player once the enemy die.
     /// The amount of money given is determined by the moneyOnDeath variable.
