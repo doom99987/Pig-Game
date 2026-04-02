0ea3c1b4e4e287b0a01233b509a3628f2d9b98a9
doom99987 <126832091+doom99987@users.noreply.github.com>
2026-04-02 13:44:29 -0700

    text

--- Assets/Scripts/GameManager/enemySpawner.cs
+++ Assets/Scripts/GameManager/enemySpawner.cs
@@ -48,7 +48,7 @@ public class enemySpawner : MonoBehaviour
         {
             int randomSpawn = Random.Range(0, spawnPoints.Length);
             int randomEnemy = Random.Range(0, enemyPrefab.Length);
-            float randSpawnDis = Random.Range(-1f, 2f);
+            int randSpawnDis = Random.Range(-2, 2);
             Instantiate(enemyPrefab[randomEnemy], spawnPoints[randomSpawn].position + new Vector3(randSpawnDis, 0, 0), spawnPoints[randomSpawn].rotation);
         }
             
