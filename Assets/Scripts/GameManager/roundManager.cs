bec524e2cb4cf417c1e3b2baf050e9c768a9002a
doom99987 <126832091+doom99987@users.noreply.github.com>
2026-04-20 13:56:40 -0700

    background

--- Assets/Scripts/GameManager/roundManager.cs
+++ Assets/Scripts/GameManager/roundManager.cs
@@ -70,7 +70,7 @@ public class roundManager : MonoBehaviour
             // Displays the time on the deathscreen
             if (deathText != null)
             {
-                deathText.text = string.Format("you had: " + "{0:0}:{1:00}", minutes, seconds + " seconds left");
+                deathText.text = string.Format("You had: " + "{0:0}:{1:00}", minutes, seconds + " seconds left");
             }
             // Checks if the round needs to be ended
             if (elapsedTime < 1)
