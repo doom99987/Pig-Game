ff13f0ead4894837ec515345c27c499a0e97d368
doom99987 <126832091+doom99987@users.noreply.github.com>
2026-05-01 14:45:32 -0700

    more audio shit im so done with this

--- Assets/Scripts/GameManager/audioManager.cs
+++ Assets/Scripts/GameManager/audioManager.cs
@@ -18,7 +18,7 @@ public class audioManager : MonoBehaviour
     void Update()
     {
         //checks if shop is open, if player is dead and decideds whether to pause, stop or unpause the music.
-        if (gameObject.GetComponent<playScenePanelManager>().getIsShopOpen())
+        if (gameObject.GetComponent<playScenePanelManager>().getIsShopOpen() || gameObject.GetComponent<gameManager>().getGameState())
         {
             gameMusic.Pause();
         }
