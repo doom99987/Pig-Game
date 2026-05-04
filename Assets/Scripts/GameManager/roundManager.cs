ed0cef827fad3220705b674d833ac992e49493b0
doom99987 <126832091+doom99987@users.noreply.github.com>
2026-05-04 14:01:50 -0700

    Revert "audio"
    This reverts commit 40b3183aedce1e8ca2e29cd0fe0de71bd47dd014.

--- Assets/Scripts/GameManager/roundManager.cs
+++ Assets/Scripts/GameManager/roundManager.cs
@@ -67,7 +67,6 @@ public class roundManager : MonoBehaviour
         // Checks if the game is paused
         if (!gameObject.GetComponent<gameManager>().getGameState() && !roundStartDelay)
         {
-            mainGpSound.Play();
             tutorialStuff.SetActive(false);
             // Updates the total time thats passed
             elapsedTime -= Time.deltaTime;
